a4fe9c2a0bb271a4fab1a365cd46e2fadac9a41f
이봉균 <bglee@rsquare.co.kr>
2017-10-08 16:34:16 +0900

    add log

--- packages/exchanges-miner/src/index.ts
+++ packages/exchanges-miner/src/index.ts
@@ -1,8 +1,19 @@
 import {store} from './store'
 import {bithumbApiTicker, BTC} from 'bit-stream'
 
+let count = 0
+
 async function get() {
   store.dispatch(bithumbApiTicker(BTC))
+  count++
 }
+
+process.on('SIGINT', () => {
+  console.log(`\nloop count: ${count}`)
+  
+  process.exit(0)
+});
+
+console.log(`start at: ${new Date().toISOString()}`)
 
 setInterval(get, 1000)
